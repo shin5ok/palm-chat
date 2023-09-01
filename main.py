b7819d49b4eb4d0b01a69531ce0ea8293009886b
shin5ok <shin5ok@55mp.com>
2023-09-02 02:01:37 +0900

    include the route for Google Chat

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import logging
 import os, sys
 import uuid
 from pydantic import BaseModel
-
 from fastapi import FastAPI, Depends, Request, Header, APIRouter
+from routers import chat
+import common
 
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler(sys.stdout)
@@ -12,15 +13,18 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 app = FastAPI()
+llm = None
 
-@app.get("/chat")
-async def _chat(request: Request, authorization = Header(default=None)) -> dict:
-    return {}
+app.include_router(
+        chat.routers,
+        prefix="/chat",
+        dependencies=[Depends(common.get_llm)]
+    )
 
 if __name__ == '__main__':
     options = {
         'port': 8080,
-        'workers': 1,
+        'workers': 2,
         'host': '0.0.0.0',
         'reload': True,
     }
