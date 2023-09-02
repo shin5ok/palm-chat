6f41fa7a27ae1375b419c586a5557e7291c45ce3
shin5ok <shin5ok@55mp.com>
2023-09-03 00:27:13 +0900

    remote some shared funcs from common

--- common.py
+++ common.py
@@ -9,7 +9,7 @@ llm = None
 memory = ConversationBufferMemory()
 
 parameters = {
-            "temperature": 0.2,
+            "temperature": 0.6,
             "max_output_tokens": 1024,
             "top_p": 0.8,
             "top_k": 40
@@ -29,27 +29,11 @@ def get_llm() -> ConversationChain:
 
     return chat_model
 
-def verify_id_token(project_number: int, authorization: str) -> bool:
-    audience = project_number
-    chat_issuer = 'chat@system.gserviceaccount.com'
-    public_cert_url_prefix = 'https://www.googleapis.com/service_accounts/v1/metadata/x509/'
-
-    token = str(authorization.split(" ")[1])
-
-    try:
-      t = client.verify_id_token(token, audience, cert_uri=public_cert_url_prefix + chat_issuer)
-
-      if t['iss'] != chat_issuer:
-        raise Exception('Invalid issuer')
-    except Exception as e:
-      print("Exception", e)
-      return False
-
-    return True
-
-
 def project():
     return {
         'id': os.environ.get('GOOGLE_CLOUD_PROJECT'),
         'number': os.environ.get('PROJECT_NUMBER'),
     }
+
+def is_debug():
+    return "DEBUG" in os.environ
