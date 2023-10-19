82b15b12262d24d8d9e11c715b7695cc03e166d7
shin5ok <shin5ok@55mp.com>
2023-10-19 09:00:43 +0900

    take slack token from env variable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,15 @@
 NAME=${1:-palm2-chat}
 PROJECT_ID=$(gcloud config get project)
 PROJECT_NUMBER=$(gcloud projects describe ${PROJECT_ID} --format="value(projectNumber)")
+SA=palm2-api@${PROJECT_ID}.iam.gserviceaccount.com
 
-gcloud run deploy \
+CMD="gcloud run deploy \
     ${NAME} \
     --region=asia-northeast1 \
     --source=. \
     --memory=1Gi \
-    --allow-unauthenticated \
-    --set-env-vars=PROJECT_NUMBER=${PROJECT_NUMBER},PROJECT_ID=${PROJECT_ID}
+    --set-env-vars=PROJECT_NUMBER=${PROJECT_NUMBER},PROJECT_ID=${PROJECT_ID},SLACK_TOKEN=${SLACK_TOKEN} \
+    --service-account=${SA} \
+    --allow-unauthenticated $@"
 
+eval $CMD
